7269dbc0f61679126c47c56bc56652af875f4fe1
dangouhaim <dangouhaim@mail.ru>
2018-07-16 11:42:56 +0300

    auth

--- auth.php
+++ auth.php
@@ -1,21 +1,49 @@
 <?
-if(isset($_POST["Login"]) && isset($_POST["Password"])))
+if(isset($_POST["Login"]) && isset($_POST["Password"]))
 {
-	setcookie("AUTH", "true");
-	header('Location: index.php');
+	$hash = MD5("admin");
+	//"d41d8cd98f00b204e9800998ecf8427e"
+	//echo $hash."<br>";
+	//echo $_POST["Password"];
+	if($_POST["Login"] == "admin" && $_POST["Password"] === $hash)
+	{
+		setcookie("AUTH", "true");
+		header('Location: index.php');
+	}
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="jquery-md5.js"></script>
+	<script type="text/javascript">
+		//var md5 = ;
+		//Create (hex-encoded) HMAC-MD5 hash of a given string value and key:
+
+		//var md5 = $.md5('value', 'key');
+		//Create raw MD5 hash of a given string value:
+
+		//var md5 = $.md5('value', null, true);
+		//Create raw HMAC-MD5 hash of a given string value and key:
+
+		//var md5 = $.md5('value', 'key', true);
+
+		$( document ).ready(function() {
+	        $("#form").submit(function()
+	        {
+	        	$("#Password").val($.md5($("#Password").val()));
+	        });
+	    });
+	</script>
 	<title></title>
 </head>
 <body>
-	<form action="auth.php" method="post">
+	<form id="form" action="auth.php" method="post">
 		<label>Login</label>
-		<input type="text" name="Login">
+		<input id="Login" type="text" name="Login">
 		<label>Password</label>
-		<input type="Password" name="Password">
+		<input id="Password" type="Password" name="Password">
 		<input type="submit" value="Login">
 	</form>
 </body>
