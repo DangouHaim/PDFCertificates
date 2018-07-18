023e23c9d7dd915548c6482833dd844a9c1bd9a6
Sashabk201 <aleksandr.boyko.93@mail.ru>
2018-07-19 01:36:08 +0300

    t code on number

--- createPdf.php
+++ createPdf.php
@@ -1,7 +1,6 @@
 <?php
 require_once( "modules/fpdf.php" );
 
-
 $cityCode = array("ABA"=>"ABAKAN","DYR"=>"ANADYR","AAQ"=>"ANAPA","WZA"=>"APATITY","ARH"=>"ARKHANGELSK","ASF"=>"ASTRAKHAN","BWO"=>"BALAKOVO","BAX"=>"BARNAUL","EGO"=>"BELGOROD","BCX"=>"BELORECK","WZC"=>"BEREZNIKI","BQS"=>"BLAGOVESCHENSK","BTK"=>"BRATSK","UUA"=>"BUGULMA","BKA"=>"BYKOVO","CSY"=>"CHEBOKSARY","CEK"=>"CHELYABINSK","CEE"=>"CHEREPOVETS","HTA"=>"CHITA","WZD"=>"EJSK","SVX"=>"EKATERINBURG","ESL"=>"ELISTA","EV"=>"EVPATORIA","GDZ"=>"GELENDZIK","GRV"=>"GROZNYJ","WZE"=>"HANTY-MANSIJSK","INA"=>"INTA","IKT"=>"IRKUTSK","IWA"=>"IVANOVO","IJK"=>"IZHEVSK","KGD"=>"KALININGRAD","KZN"=>"KAZAN","KEJ"=>"KEMEROVO","KE"=>"KERCH","KHV"=>"KHABAROVSK","WZT"=>"KHIBINY","KVX"=>"KIROV","KVK"=>"KIROVSK","KGP"=>"KOGALYN","WZH"=>"KOLKHI","KXK"=>"KOMSOMOLSK-NA-AMURE","WZI"=>"KRAJNIJ","KRR"=>"KRASNODAR","KJA"=>"KRASNOJARSK","KRO"=>"KURGAN","KUR"=>"KURSK","LPK"=>"LIPETSK","GDX"=>"MAGADAN","MQF"=>"MAGNITOGORSK","WZJ"=>"MAJKOP","MCX"=>"MAKHACHKALA","MRV"=>"MINERALNYE-VODY","MJZ"=>"MIRNYJ","MOW"=>"MOSCOW","MMK"=>"MURMANSK","NBC"=>"NABEREVOYE-CHELNYB","NYM"=>"NADYM","WZL"=>"NAKHICHEVAN","NAL"=>"NALCHIK","NNM"=>"NARYAN-MAR","IGT"=>"NAZRAN","NFG"=>"NEFTEYUGANSK","NER"=>"NERYUNGRI","NJC"=>"NIZHNEVARTOVSK","GOJ"=>"NIZHNIY-NOVGOROD","NOJ"=>"NOJABRXSK","NSK"=>"NORILSK","GNO"=>"NOVGOROD-THE-GREAT","NOZ"=>"NOVOKUZNETSK","OVB"=>"NOVOSIBIRSK","NUX"=>"NOVYJ-URENGOJ","WZM"=>"NYAGAN","OMS"=>"OMSK","REN"=>"ORENBURG","OSW"=>"ORSK","PEZ"=>"PENZA","PEE"=>"PERM","PKC"=>"PETROPAVLOVSK-KAMCHA","PES"=>"PETROZAVODSK","PWE"=>"PEVEK","PYJ"=>"POLYARNYY","PTG"=>"PYATIGORSK","RAT"=>"RADUZHNYI","ROV"=>"ROSTOV","SLY"=>"SALEKHARD","KUF"=>"SAMARA","SKX"=>"SARANSK","RTW"=>"SARATOV","SIP"=>"SIMFEROPOL","WZN"=>"SLEPCOVSKAYA","AER"=>"SOCHI/ADLER","WZO"=>"SOKOL","LED"=>"ST.PETERSBURG","WZP"=>"STARYJ-OSKOL","STW"=>"STAVROPOL","SWT"=>"STRZHEWOI","SGC"=>"SURGUT","SUZ"=>"SUZDAL","SCW"=>"SYKTYVKAR","IKS"=>"TIKSI","TOF"=>"TOMSK","TVE"=>"TVER","TJM"=>"TYUMEN","UFA"=>"UFA","UCT"=>"UKHTA","UUD"=>"ULAN-UDE","ULY"=>"ULYANOVSK","USK"=>"USINSK","UIK"=>"UST-ILIMSK","OGZ"=>"VLADIKAVKAZ","VVO"=>"VLADIVOSTOK","VLK"=>"VOLGODONSK","VOG"=>"VOLGOGRAD","VGD"=>"VOLOGDA","VKT"=>"VORKUTA","VOZ"=>"VORONEZH","YKS"=>"YAKKUTSK",);
 
 $MonthArray = array("January","February","March","April","May","June","July","August","September","October","November","December");
@@ -22,6 +21,21 @@ $Curses = array(
 );
 
 
+$tcode = array(
+	"1. FMT Basic" => 'T1',
+	"2. FMT Basic+Perfomance" => "TF",
+	"3. FMT Perfomance" => "T2",
+	"4. FMT Blades" => "T2",
+	"5. FMT Blades Advanced" => "T2",
+	"6. FMT SCN" => "T2",
+	"7. FMT SCN+MOV" => "T2",
+	"8. FMT MOV" => "T2",
+	"9. FMT Logopedics" => "T2",
+	"10. FMT Dent" => "T2",
+	"11. FMT Equine" => "T2",
+	"12. FMT Special Course" => "T2"
+);
+
 if(isset($_POST['name']) && isset($_POST['Country']) && isset($_POST['City']) && isset($_POST['Day']) && isset($_POST['Mounth']) && isset($_POST['Year']) && isset($_POST['Curs'])) {
 
 	$city = $_POST['City'];
@@ -37,7 +51,7 @@ if(isset($_POST['name']) && isset($_POST['Country']) && isset($_POST['City']) &&
 	$i = 0;
 
 	$pdf = new FPDF('L','mm','A4');
-	foreach (GenerateNumbers($names,$city,$mount,$year,"IT1") as $num) {
+	foreach (GenerateNumbers($names,$city,$mount,$year,$tcode[$curs]) as $num) {
 		FmtBasic($pdf,$num,$names[$i],$cityCode[$city],$contry,$MonthArray[$mount-1],$day,"20".$year,$Curses[$curs]);
 		$i++;
 	}
